199a78e6b5e8f745376d55e482d8d6b1a2549b20
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-18 17:13:45 -0500

    added requirements.txt

--- obd-ii-reader.py
+++ obd-ii-reader.py
@@ -1,7 +1,6 @@
 #!/bin/python3 from pyobd.obd_io import OBDConnection
 
-import os
-import pyobd.modified_obd_io
+from obd.obd import OBD
 PORT = 3
 
-connection = OBDConnection(PORT, 'AUTO', 1000, 5, False)
+connection = OBD()

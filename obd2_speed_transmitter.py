b4194cec345c410162afa1c5d69a34b169cd78d2
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-19 03:21:43 -0500

    added port scanning

--- obd2_speed_transmitter.py
+++ obd2_speed_transmitter.py
@@ -9,18 +9,24 @@ from time import sleep
 from obd_ii_reader import OBD2SpeedGet
 
 class OBD2SpeedTransmitter:
-    def __init__(self, com_port, baudrate=9600, timeout=0.5):
-        self.ser = Serial(port=str(com_port), baudrate=baudrate, timeout=timeout)
-        self.obd2_speed_get = OBD2SpeedGet()
+	def __init__(self, com_port=None, baudrate=9600, timeout=0.5):
+		if com_port is None:
+			com_port = scan_ports()[0]
+		self.ser = Serial(port=str(com_port), baudrate=baudrate, timeout=timeout)
+		self.obd2_speed_get = OBD2SpeedGet()
 
-    def send_speed(self):
-        self.ser.write((self.obd2_speed_get.get_speed_str() + '\n').encode())
+	def send_speed(self):
+		self.ser.write((self.obd2_speed_get.get_speed_str() + '\n').encode())
 
 def __main__():
-    transmitter = OBD2SpeedTransmitter("/dev/ttyUSB0")
-    while True:
-        transmitter.send_speed();
-        sleep(0.1);
+	# There should only be two options here...
+	try:
+		transmitter = OBD2SpeedTransmitter("/dev/ttyUSB0")
+	except Exception:
+		transmitter = OBD2SpeedTransmitter("/dev/ttyUSB1")
+	while True:
+		transmitter.send_speed();
+		sleep(0.1);
 
 if __name__ == "__main__":
     __main__()

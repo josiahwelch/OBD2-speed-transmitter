7cb7f963ff9ffab8a7b0ff5134406077b3142be0
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-19 01:36:14 -0500

    added main function to obd2-speed-transmitter.py

--- obd2-speed-transmitter.py
+++ obd2-speed-transmitter.py
@@ -1,7 +1,19 @@
 #!/bin/python3
 
 import pyserial
+import time
+from obd-ii-reader import OBD2SpeedGet
 
 class OBD2SpeedTransmitter:
 	__init__(com_port, baudrate=9600, timeout=0.5):
         self.ser = Serial(port=com_port, baudrate, timeout)
+        self.obd2_speed_get = OBD2SpeedGet()
+
+    def send_speed():
+        self.ser.write(self.get_speed_str() + '\n')
+
+def __main__:
+    transmitter = OBD2SpeedTransmitter("/dev/ttyUSB0")
+    while True:
+        transmitter.send_speed();
+        sleep(0.1);
